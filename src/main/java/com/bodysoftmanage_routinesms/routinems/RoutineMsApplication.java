e038d4ea3c5cc3446ae8faac3c41707303baaeb8
unknown <afsanchezsa@unal.edu.co>
2020-04-03 14:10:48 -0500

    initial project

---
+++ src/main/java/com/bodysoftmanage_routinesms/routinems/RoutineMsApplication.java
@@ -0,0 +1,13 @@
+package com.bodysoftmanage_routinesms.routinems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutineMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutineMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bodysoftmanage_routinesms/routinems/RoutineMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodysoftmanage_routinesms.routinems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
