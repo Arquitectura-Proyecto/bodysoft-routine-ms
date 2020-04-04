c531304eccf41785a0e85b11a9a101d24d251032
unknown <afsanchezsa@unal.edu.co>
2020-04-03 19:46:54 -0500

    status and typeresource model

--- src/main/java/com/bodysoftmanage_routinesms/routinems/model/Status.java
+++ src/main/java/com/bodysoftmanage_routinesms/routinems/model/Status.java
@@ -1,4 +1,55 @@
 package com.bodysoftmanage_routinesms.routinems.model;
 
-public class Status {
+import org.apache.catalina.User;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name="status",schema = "public")
+public class Status implements Serializable {
+    private static final long serialVersionUID=1L;
+/**
+ * Attributes
+ */
+    private Integer id;
+
+    private String name;
+
+    private List<UserRoutine> userRoutines;
+
+    public Status(Integer id, String name, List<UserRoutine> userRoutines) {
+        this.id = id;
+        this.name = name;
+        this.userRoutines = userRoutines;
+    }
+
+    public Status() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<UserRoutine> getUserRoutines() {
+        return userRoutines;
+    }
+
+    public void setUserRoutines(List<UserRoutine> userRoutines) {
+        this.userRoutines = userRoutines;
+    }
 }
--- src/main/java/com/bodysoftmanage_routinesms/routinems/model/TypeResource.java
+++ src/main/java/com/bodysoftmanage_routinesms/routinems/model/TypeResource.java
@@ -1,4 +1,52 @@
 package com.bodysoftmanage_routinesms.routinems.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name="type",schema = "public")
 public class TypeResource {
+    private static final long serialVersionUID=1L;
+/**
+ * Attributes
+ */
+    private Integer id;
+
+    private String name;
+
+    private List<Routine> routines;
+
+    public TypeResource(Integer id, String name, List<Routine> routines) {
+        this.id = id;
+        this.name = name;
+        this.routines = routines;
+    }
+
+    public TypeResource() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Routine> getRoutines() {
+        return routines;
+    }
+
+    public void setRoutines(List<Routine> routines) {
+        this.routines = routines;
+    }
 }
