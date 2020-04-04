fd910a4b99b6270c1087377bc872a0e9c423bf15
unknown <afsanchezsa@unal.edu.co>
2020-04-03 20:00:51 -0500

    userroutine model

--- src/main/java/com/bodysoftmanage_routinesms/routinems/model/UserRoutine.java
+++ src/main/java/com/bodysoftmanage_routinesms/routinems/model/UserRoutine.java
@@ -1,4 +1,50 @@
 package com.bodysoftmanage_routinesms.routinems.model;
 
-public class UserRoutine {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name="user_routine")
+public class UserRoutine implements Serializable {
+    private static final long serialVersionUID=1L;
+/**
+ * Attributes
+ */
+    private Integer idUser;
+    private Routine routine;
+    private Status status;
+
+    public UserRoutine(Integer idUser, Routine routine, Status status) {
+        this.idUser = idUser;
+        this.routine = routine;
+        this.status = status;
+    }
+
+    public UserRoutine() {
+    }
+
+    public Integer getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Integer idUser) {
+        this.idUser = idUser;
+    }
+
+    public Routine getRoutine() {
+        return routine;
+    }
+
+    public void setRoutine(Routine routine) {
+        this.routine = routine;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
