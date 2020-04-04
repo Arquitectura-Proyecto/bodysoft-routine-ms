2376954dfd74897db8249e18485b8a0c415acca5
unknown <afsanchezsa@unal.edu.co>
2020-04-04 12:43:08 -0500

    registerRoutine in Controller implemented

--- src/main/java/com/bodysoftmanage_routinesms/routinems/controller/RoutineController.java
+++ src/main/java/com/bodysoftmanage_routinesms/routinems/controller/RoutineController.java
@@ -4,6 +4,7 @@ import com.bodysoftmanage_routinesms.routinems.model.Routine;
 import com.bodysoftmanage_routinesms.routinems.model.TypeRoutine;
 import com.bodysoftmanage_routinesms.routinems.pojo.RegisterRoutinePOJO;
 import com.bodysoftmanage_routinesms.routinems.service.RoutineService;
+import com.bodysoftmanage_routinesms.routinems.service.TypeRoutineService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,20 +12,32 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RestController
 public class RoutineController {
+    private static float DefaultRating= (float) 0.0;
+    private  static Integer DefaultNumRaitings=0;
     private RoutineService routineService;
-    private TypeRoutine typeRoutine;
+    private TypeRoutineService typeRoutineService;
 
-    public RoutineController(RoutineService routineService) {
+    public RoutineController(RoutineService routineService, TypeRoutineService typeRoutineService) {
         this.routineService = routineService;
-
+        this.typeRoutineService = typeRoutineService;
     }
 
     @PostMapping(value={"register/routine/{idType}"})
     public ResponseEntity registerNewRoutine(@PathVariable Integer idType, @RequestBody RegisterRoutinePOJO routinePOJO){
-    Routine newRoutine=new Routine();
+    TypeRoutine type=typeRoutineService.findById(idType);
+
+        if(type==null && !routineService.isRigthRoutine(routinePOJO)){
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
+        Routine newRoutine=new Routine();
     newRoutine.setDescription(routinePOJO.getDescription());
     newRoutine.setPrice(routinePOJO.getPrice());
     newRoutine.setName(routinePOJO.getName());
+    newRoutine.setLinkPreview(routinePOJO.getLink_preview());
+    newRoutine.setIdOwner(routinePOJO.getIdOwner());
+    newRoutine.setRating(DefaultRating);
+    newRoutine.setType(type);
+    newRoutine.setNumRaitings(DefaultNumRaitings);
     routineService.save(newRoutine);
     return new ResponseEntity(HttpStatus.CREATED);
     }
---
+++ src/main/java/com/bodysoftmanage_routinesms/routinems/service/TypeRoutineService.java
@@ -0,0 +1,21 @@
+package com.bodysoftmanage_routinesms.routinems.service;
+
+import com.bodysoftmanage_routinesms.routinems.model.TypeRoutine;
+import com.bodysoftmanage_routinesms.routinems.repository.TypeRoutineRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TypeRoutineService {
+    private TypeRoutineRepository typeRoutineRepository;
+
+    public TypeRoutineService(TypeRoutineRepository typeRoutineRepository) {
+        this.typeRoutineRepository = typeRoutineRepository;
+    }
+    public void save(TypeRoutine type){
+        this.typeRoutineRepository.save(type);
+    }
+    public TypeRoutine findById(Integer id ){
+        return this.typeRoutineRepository.findById(id).orElse(null);
+    }
+}
+
