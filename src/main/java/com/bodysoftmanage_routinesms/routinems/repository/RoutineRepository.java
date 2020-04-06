2522473230db369219dc8e136e89e8429a1230a3
unknown <afsanchezsa@unal.edu.co>
2020-04-06 00:41:19 -0500

    update and queries of routines implemented

--- src/main/java/com/bodysoftmanage_routinesms/routinems/repository/RoutineRepository.java
+++ src/main/java/com/bodysoftmanage_routinesms/routinems/repository/RoutineRepository.java
@@ -4,7 +4,10 @@ import com.bodysoftmanage_routinesms.routinems.model.Routine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoutineRepository extends JpaRepository<Routine,Integer> {
-
+    public List<Routine> findAllByIdOwner(Integer idOwner);
+    public List<Routine> findAllByTypeId(Integer idType);
 }
--- src/main/java/com/bodysoftmanage_routinesms/routinems/service/RoutineService.java
+++ src/main/java/com/bodysoftmanage_routinesms/routinems/service/RoutineService.java
@@ -5,6 +5,8 @@ import com.bodysoftmanage_routinesms.routinems.pojo.RegisterRoutinePOJO;
 import com.bodysoftmanage_routinesms.routinems.repository.RoutineRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RoutineService {
     private RoutineRepository routineRepository;
@@ -26,6 +28,23 @@ public class RoutineService {
                     &&!routine.getName().trim().isEmpty()&&routine.getPrice()>=0;
         }
         return  correct;
+    }
+    public boolean isOwner(Integer idRoutine,Integer idOwner){
+        Routine routine= routineRepository.findById(idRoutine).orElse(null);
+        return routine.getIdOwner().compareTo(idOwner)==0;
+
+    }
+    public List<Routine> getAllByIdOwner(Integer idOwner){
+        return routineRepository.findAllByIdOwner(idOwner);
+    }
+    public Routine getById(Integer id){
+        return this.routineRepository.findById(id).orElse(null);
+    }
+    public List<Routine>getAll(){
+        return this.routineRepository.findAll();
+    }
+    public List<Routine>getByType(Integer idType){
+        return routineRepository.findAllByTypeId(idType);
     }
 
 }
